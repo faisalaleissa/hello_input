a7163244468f7f23cc06e05c65454f511eaee7f8
faisalaleissa <31824833+faisalaleissa@users.noreply.github.com>
2018-05-07 12:28:07 +0300

    comm

--- Hello_world.py
+++ Hello_world.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
 def print_my_name(name):
 	return name
 
+listOfArguments = sys.argv[1:]
+Name = str(listOfArguments[0])
 
-print_my_name('Faisal Aleissa')
+print_my_name(Name)
